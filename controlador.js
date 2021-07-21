da327d51ed43c265f93d07a7bd64dd65b395a52c
jjosegallegocesde <61057518+jjosegallegocesde@users.noreply.github.com>
2021-07-21 16:52:56 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,10 @@
+//Programa que nos ayude a establecer
+//el costo total de una consola de video juegos
+
+//En el exterior
+//compra en dolares
+//Pagar un casillero 
+//(dolares costo dependiendo de la cantidad de productos)
+//envio hasta colombia
+//(cantidad de productos)
+//pagar impuesto de entrada al pais (lo digita aduanas)
